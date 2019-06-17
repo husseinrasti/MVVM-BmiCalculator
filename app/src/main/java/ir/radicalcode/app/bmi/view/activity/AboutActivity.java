f7075f241c8c52eb5dafd4b3d241128ee54d61fa
Hussein Rasti <husseinrasti@gmail.com>
2019-06-18 02:41:47 +0430

    Added Activity About & Resources

--- app/src/main/java/ir/radicalcode/app/bmi/view/activity/AboutActivity.java
+++ app/src/main/java/ir/radicalcode/app/bmi/view/activity/AboutActivity.java
@@ -1,15 +1,36 @@
 package ir.radicalcode.app.bmi.view.activity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import ir.radicalcode.app.bmi.R;
 
 public class AboutActivity extends AppCompatActivity {
+
+    private Unbinder unbinder;
+
+    @BindView(R.id.txtEmail)
+    TextView email;
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_about );
+        unbinder = ButterKnife.bind( this );
+
+        email.setText( Html.fromHtml( "<a href=\"mailto:teachcode.ir@gmail.com\">ارسال نظر و پیشنهاد</a>" ) );
+        email.setMovementMethod( LinkMovementMethod.getInstance() );
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
     }
 }

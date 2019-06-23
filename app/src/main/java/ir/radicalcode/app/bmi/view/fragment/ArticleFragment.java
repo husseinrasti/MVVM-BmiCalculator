8a2ffc5cd345a0660ea8045ba0af92d7471721c7
Hussein Rasti <husseinrasti@gmail.com>
2019-06-23 11:59:10 +0430

    Modify layout article to webview

--- app/src/main/java/ir/radicalcode/app/bmi/view/fragment/ArticleFragment.java
+++ app/src/main/java/ir/radicalcode/app/bmi/view/fragment/ArticleFragment.java
@@ -8,18 +8,11 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProviders;
 import butterknife.ButterKnife;
 import ir.radicalcode.app.bmi.R;
-import ir.radicalcode.app.bmi.root.Injection;
 import ir.radicalcode.app.bmi.utils.Const;
-import ir.radicalcode.app.bmi.view.viewmodel.BmiViewModel;
-import ir.radicalcode.app.bmi.view.viewmodel.FactoryViewModel;
 
 public class ArticleFragment extends Fragment {
-
-    private FactoryViewModel factoryViewModel;
-    private BmiViewModel bmiViewModel;
 
     public static ArticleFragment newInstance( int pageNo ) {
         Bundle args = new Bundle();
@@ -34,16 +27,12 @@ public class ArticleFragment extends Fragment {
         super.onCreate( savedInstanceState );
         assert getArguments() != null;
         int mPageNo = getArguments().getInt( Const.ARG_PAGE );
-        factoryViewModel = Injection.provideBMIViewModelFactory( getActivity() );
-        bmiViewModel = ViewModelProviders.of( this , factoryViewModel ).get( BmiViewModel.class );
     }
 
     @Override
-    public View onCreateView( @NonNull LayoutInflater inflater , ViewGroup container ,
-                              Bundle savedInstanceState ) {
+    public View onCreateView( @NonNull LayoutInflater inflater , ViewGroup container , Bundle savedInstanceState ) {
         View view = inflater.inflate( R.layout.fragment_article , container , false );
         ButterKnife.bind( this , view );
         return view;
     }
-
 }
